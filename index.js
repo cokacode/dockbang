1825606ebb7f17418c6444dff8e0e00f4290156c
cokacode <test@gmail.com>
2022-10-24 11:57:04 +0530

    Added Static Files

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express"
-import {join} from "path"
+import {join, dirname} from "path"
+import { fileURLToPath } from 'url';
+
 
 const app = express();
-app.use(express.static(join(process.cwd, "static")))
+app.use(express.static(join( dirname(fileURLToPath(import.meta.url)), "static")))
 
 app.listen(8000, ()=>{
     console.log("Listining at port 8000");
